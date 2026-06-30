2455ae50d289452a94965bab8c27ba6a73498ef1
Hahyun Song <songhahyun551@gmail.com>
2026-06-30 16:06:01 +0900

    update model train parameters

--- src/config.py
+++ src/config.py
@@ -52,7 +52,7 @@ class Config:
 
     # dataloader
     pretokenize_dataset: bool = True
-    num_workers: int = 6
+    num_workers: int = 8
     pin_memory: bool = True
     persistent_workers: bool = True
 
